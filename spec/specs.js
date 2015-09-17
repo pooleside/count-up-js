9cbcadb1dd5ed031cbfa1f15c5da725688a95104
pooleside <lpoole26@gmail.com>
2015-09-17 13:47:46 -0700

    "first spec written"

--- spec/specs.js
+++ spec/specs.js
@@ -1,1 +1,5 @@
-
+describe('countDivisor', function() {
+  it("it counts to a given number 'y' in increments of given number 'x'", function() {
+    expect(countDivisor(3, 9)).to.equal(3,6,9);
+  });
+})
